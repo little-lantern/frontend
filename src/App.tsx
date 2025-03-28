1bfda06d01179aa189ee40d1dd003c636eb92bb8
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2025-03-28 18:00:00 +0530

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomePage from "./pages/Home.page";
-// import AboutPage from "./pages/About.page";
+import AboutPage from "./pages/About.page";
 import "./App.scss";
-// import NotFound from "./pages/NotFound";
-// import ExplorePlayLearnComponent from "./components/Homepage/ExplorePlayLearn.component";
-// import ContactPage from "./pages/Contact.page";
-// import ProgramsPage from "./pages/Programs.page";
-// import WorkWithUsPage from "./pages/WorkWithUs.page";
-// import ProgramOverviewPage from "./pages/ProgramOverview.page";
-// import AdmissionsPage from "./pages/Admissions.page";
-// import ActivityHubPage from "./pages/ActivityHub.page";
-// import OurSpacePage from "./pages/OurSpace.page";
-// import LearningPage from "./pages/Learning.page";
-// import ApplyNowPage from "./pages/ApplyNow.page";
-// import PrivacyPolicyPage from "./pages/PrivacyPolicy.page";
-// import TermsOfUsePage from "./pages/TermsOfUse.page";
-// import ScrollToTop from "./components/ScrollToTop.component";
-// import ThankYouPage from "./pages/ThankYou.page";
-// import ErrorPage from "./pages/Error.page";
+import NotFound from "./pages/NotFound";
+import ExplorePlayLearnComponent from "./components/Homepage/ExplorePlayLearn.component";
+import ContactPage from "./pages/Contact.page";
+import ProgramsPage from "./pages/Programs.page";
+import WorkWithUsPage from "./pages/WorkWithUs.page";
+import ProgramOverviewPage from "./pages/ProgramOverview.page";
+import AdmissionsPage from "./pages/Admissions.page";
+import ActivityHubPage from "./pages/ActivityHub.page";
+import OurSpacePage from "./pages/OurSpace.page";
+import LearningPage from "./pages/Learning.page";
+import ApplyNowPage from "./pages/ApplyNow.page";
+import PrivacyPolicyPage from "./pages/PrivacyPolicy.page";
+import TermsOfUsePage from "./pages/TermsOfUse.page";
+import ScrollToTop from "./components/ScrollToTop.component";
+import ThankYouPage from "./pages/ThankYou.page";
+import ErrorPage from "./pages/Error.page";
 const App: React.FC = () => {
   return (
     <Router>
-      {/* <ScrollToTop /> */}
-      {/* <Header /> */}
+      <ScrollToTop />
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/programs" element={<ProgramsPage />} />
@@ -44,11 +43,11 @@ const App: React.FC = () => {
           <Route path="/terms-of-use" element={<TermsOfUsePage />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-      {/* <ExplorePlayLearnComponent /> */}
-      {/* <Footer /> */}
+      <ExplorePlayLearnComponent />
+      <Footer />
     </Router>
   );
 };
