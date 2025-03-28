a02f8fafb9d312058ac045ed27efc558f9b3b473
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2025-03-28 17:48:09 +0530

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,33 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/Home.page";
-import AboutPage from "./pages/About.page";
+// import AboutPage from "./pages/About.page";
 import "./App.scss";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
 import ExplorePlayLearnComponent from "./components/Homepage/ExplorePlayLearn.component";
-import ContactPage from "./pages/Contact.page";
-import ProgramsPage from "./pages/Programs.page";
-import WorkWithUsPage from "./pages/WorkWithUs.page";
-import ProgramOverviewPage from "./pages/ProgramOverview.page";
-import AdmissionsPage from "./pages/Admissions.page";
-import ActivityHubPage from "./pages/ActivityHub.page";
-import OurSpacePage from "./pages/OurSpace.page";
-import LearningPage from "./pages/Learning.page";
-import ApplyNowPage from "./pages/ApplyNow.page";
-import PrivacyPolicyPage from "./pages/PrivacyPolicy.page";
-import TermsOfUsePage from "./pages/TermsOfUse.page";
-import ScrollToTop from "./components/ScrollToTop.component";
-import ThankYouPage from "./pages/ThankYou.page";
-import ErrorPage from "./pages/Error.page";
+// import ContactPage from "./pages/Contact.page";
+// import ProgramsPage from "./pages/Programs.page";
+// import WorkWithUsPage from "./pages/WorkWithUs.page";
+// import ProgramOverviewPage from "./pages/ProgramOverview.page";
+// import AdmissionsPage from "./pages/Admissions.page";
+// import ActivityHubPage from "./pages/ActivityHub.page";
+// import OurSpacePage from "./pages/OurSpace.page";
+// import LearningPage from "./pages/Learning.page";
+// import ApplyNowPage from "./pages/ApplyNow.page";
+// import PrivacyPolicyPage from "./pages/PrivacyPolicy.page";
+// import TermsOfUsePage from "./pages/TermsOfUse.page";
+// import ScrollToTop from "./components/ScrollToTop.component";
+// import ThankYouPage from "./pages/ThankYou.page";
+// import ErrorPage from "./pages/Error.page";
 const App: React.FC = () => {
   return (
     <Router>
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/programs" element={<ProgramsPage />} />
@@ -43,7 +44,7 @@ const App: React.FC = () => {
           <Route path="/terms-of-use" element={<TermsOfUsePage />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </main>
       <ExplorePlayLearnComponent />
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
-import TopBannerComponent from "../components/Homepage/TopBanner.component";
-import InfoStripComponent from "../components/Homepage/InfoStrip.component";
-import OurSpaceComponent from "../components/Homepage/OurSpace.component";
-import OurProgramsComponents from "../components/Homepage/OurPrograms.component";
-import ZoneAndAreaComponent from "../components/Homepage/ZoneAndArea.component";
-// import VideoEmbedComponent from "../components/Homepage/VideoEmbed.component";
-import LearningsComponent from "../components/Homepage/Learnings.component";
-import ActivityHubComponent from "../components/Homepage/ActivityHub.component";
-import AdmissionComponent from "../components/Homepage/Admission.component";
-import InstagramFeedComponent from "../components/Homepage/InstagramFeed.component";
+// import TopBannerComponent from "../components/Homepage/TopBanner.component";
+// import InfoStripComponent from "../components/Homepage/InfoStrip.component";
+// import OurSpaceComponent from "../components/Homepage/OurSpace.component";
+// import OurProgramsComponents from "../components/Homepage/OurPrograms.component";
+// import ZoneAndAreaComponent from "../components/Homepage/ZoneAndArea.component";
+// import LearningsComponent from "../components/Homepage/Learnings.component";
+// import ActivityHubComponent from "../components/Homepage/ActivityHub.component";
+// import AdmissionComponent from "../components/Homepage/Admission.component";
+// import InstagramFeedComponent from "../components/Homepage/InstagramFeed.component";
 
 const HomePage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <TopBannerComponent />
+      <h1>coming soon...</h1>
+      {/* <TopBannerComponent />
       <InfoStripComponent />
       <OurSpaceComponent />
       <OurProgramsComponents />
       <ZoneAndAreaComponent />
       {/* <VideoEmbedComponent /> */}
-      <LearningsComponent />
+      {/* <LearningsComponent />
       <ActivityHubComponent />
       <AdmissionComponent />
-      <InstagramFeedComponent />
+      <InstagramFeedComponent /> */}
     </div>
   );
 };
