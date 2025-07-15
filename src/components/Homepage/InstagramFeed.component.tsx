fe1b4ce90a36bc2ceddc9d2f4ee7b3097da5b040
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2025-07-15 14:06:29 +0530

    update code

--- src/components/Homepage/InstagramFeed.component.tsx
+++ src/components/Homepage/InstagramFeed.component.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import styles from "./InstagramFeed.component.module.scss";
 import { Link } from "react-router-dom";
-import instaImg from "../../assets/images/insta-feed-01.png";
-import instagramIcon from "../../assets/images/instagram-icon.png";
+// import instaImg from "../../assets/images/insta-feed-01.png";
+// import instagramIcon from "../../assets/images/instagram-icon.png";
 
 const InstagramFeedComponent: React.FC = () => {
   return (
